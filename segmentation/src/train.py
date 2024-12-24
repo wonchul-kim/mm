4712b5f510a0985aa519d7c7fe619e765405eea7
wonchul <kim.wonchul@aiv.ai>
2024-12-24 15:50:57 +0900

    train.py updated

--- segmentation/src/train.py
+++ segmentation/src/train.py
@@ -13,18 +13,47 @@ from pathlib import Path
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[2]
 
-def main():
-
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train a segmentor')
+    parser.add_argument(
+        '--cfg-options',
+        nargs='+',
+        action=DictAction,
+        help='override some settings in the used config, the key-value pair '
+        'in xxx=yyy format will be merged into config file. If the value to '
+        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+        'Note that the quotation marks are necessary and that no white space '
+        'is allowed.')
+    parser.add_argument(
+        '--launcher',
+        choices=['none', 'pytorch', 'slurm', 'mpi'],
+        default='none',
+        help='job launcher')
+    # When using PyTorch version >= 2.0.0, the `torch.distributed.launch`
+    # will pass the `--local-rank` parameter to `tools/train.py` instead
+    # of `--local_rank`.
+    parser.add_argument('--local_rank', '--local-rank', type=int, default=0)
+    args = parser.parse_args()
     if 'LOCAL_RANK' not in os.environ:
-        os.environ['LOCAL_RANK'] = '0'
+        os.environ['LOCAL_RANK'] = str(args.local_rank)
+
+    return args
+
+def main():
+    args = parse_args()
 
     config_file = ROOT / 'segmentation/configs/models/mask2former/mask2former_swin-l-in22k-384x384-pre_8xb2-160k_ade20k-640x640.py'
     amp = True
-    cfg_options = {'load_from': '/HDD/weights/mmseg/mask2former/mask2former_swin-l-in22k-384x384-pre_8xb2-160k_ade20k-640x640_20221203_235933-7120c214.pth',
-                   'launcher': 'none', 
+    if args.cfg_options is None:
+        cfg_options = {'load_from': '/HDD/weights/mmseg/mask2former/mask2former_swin-l-in22k-384x384-pre_8xb2-160k_ade20k-640x640_20221203_235933-7120c214.pth',
+                   'launcher': args.launcher, 
                    'resume': False,
                    'work_dir': '/HDD/datasets/projects/LX/24.12.12/outputs/mm'
             }
+    else:
+        cfg_options = args.cfg_options
+        
     cfg = Config.fromfile(config_file)
     cfg.merge_from_dict(cfg_options)
     
