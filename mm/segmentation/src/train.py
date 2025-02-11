b09bab22cb461421eaa8527a5d03f6a2689e0ba3
wonchul <kim.wonchul@aiv.ai>
2025-02-11 19:12:28 +0900

    added swin-b-in22k, swin-l-in22k

--- mm/segmentation/src/train.py
+++ mm/segmentation/src/train.py
@@ -141,7 +141,7 @@ def main2():
     args.num_classes = len(classes)
     
     args.model= 'mask2former'
-    args.backbone = 'swin-b-in1k'
+    args.backbone = 'swin-l-in22k'
     args.height = 256
     args.width = 512
     
