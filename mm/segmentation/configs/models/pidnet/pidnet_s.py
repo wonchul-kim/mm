cc4816003476a1c245bb0483b7479a7e22b2c543
wonchul <kim.wonchul@aiv.ai>
2025-02-25 10:44:25 +0900

    apply frozen_stages to pidnet

--- mm/segmentation/configs/models/pidnet/pidnet_s.py
+++ mm/segmentation/configs/models/pidnet/pidnet_s.py
@@ -70,23 +70,6 @@ model = dict(
     train_cfg=dict(),
     test_cfg=dict(mode='whole'))
 
-# train_pipeline = [
-#     dict(type='LoadImageFromFile'),
-#     dict(type='LoadAnnotations'),
-#     dict(
-#         type='RandomResize',
-#         scale=(2048, 1024),
-#         ratio_range=(0.5, 2.0),
-#         keep_ratio=True),
-#     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
-#     dict(type='RandomFlip', prob=0.5),
-#     dict(type='PhotoMetricDistortion'),
-#     dict(type='GenerateEdge', edge_width=4),
-#     dict(type='PackSegInputs')
-# ]
-# train_dataloader = dict(batch_size=6, dataset=dict(pipeline=train_pipeline))
-
-iters = 120000
 # optimizer
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
 optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
@@ -97,15 +80,10 @@ param_scheduler = [
         eta_min=0,
         power=0.9,
         begin=0,
-        end=iters,
+        end=120000,
         by_epoch=False)
 ]
 
-# # training schedule for 120k
-# train_cfg = dict(
-#     type='IterBasedTrainLoop', max_iters=iters, val_interval=iters // 10)
-# val_cfg = dict(type='ValLoop')
-# test_cfg = dict(type='TestLoop')
 # default_hooks = dict(
 #     timer=dict(type='IterTimerHook'),
 #     logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
