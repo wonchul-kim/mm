54a71ddbe4d428f86918ac9fafba6d2972510a1a
wonchul <kim.wonchul@aiv.ai>
2025-03-04 16:44:17 +0900

    split tta into modules

--- mm/segmentation/src/loops/test_loop.py
+++ mm/segmentation/src/loops/test_loop.py
@@ -6,6 +6,120 @@ from mmengine.registry import LOOPS
 from mmengine.runner.amp import autocast
 from mmengine.runner.loops import TestLoop, _parse_losses, _update_losses
 
+
+from torchvision.transforms import RandomHorizontalFlip, RandomVerticalFlip, RandomRotation
+from mmengine.structures import PixelData
+
+def merge_tta_batch(batch_output, out_channels, decode_head_threshold=None):
+    seg_logits = batch_output[0].seg_logits.data
+    logits = torch.zeros(seg_logits.shape).to(seg_logits)
+    for data_sample in batch_output:
+        seg_logit = data_sample.seg_logits.data
+        if out_channels > 1:
+            logits += seg_logit.softmax(dim=0)
+        else:
+            logits += seg_logit.sigmoid()
+    logits /= len(batch_output)
+    if out_channels == 1:
+        seg_pred = (logits > decode_head_threshold).to(logits).squeeze(1)
+    else:
+        seg_pred = logits.argmax(dim=0)
+    data_sample.set_data({'seg_logits': PixelData(data=logits)})
+    data_sample.set_data({'pred_sem_seg': PixelData(data=seg_pred)})
+    if hasattr(batch_output[0], 'gt_sem_seg'):
+        data_sample.set_data(
+            {'gt_sem_seg': batch_output[0].gt_sem_seg})
+    data_sample.set_metainfo({'img_path': batch_output[0].img_path})
+    
+    return data_sample
+            
+
+def make_tta_batch(augs, new_data_batch, batch_input, batch_data_sample):
+    new_data_batch['inputs'].append(batch_input)
+    new_data_batch['data_samples'].append(batch_data_sample)
+    for key, val in augs.items():
+        if key == 'HorizontalFlip' and val:
+            new_data_batch['inputs'].append(RandomHorizontalFlip(1)(batch_input))
+            new_data_batch['data_samples'].append(batch_data_sample)
+        
+        if key == 'VerticalFlip' and val:
+            new_data_batch['inputs'].append(RandomVerticalFlip(1)(batch_input))
+            new_data_batch['data_samples'].append(batch_data_sample)
+            
+        if key == 'Rotate' and val:
+            if isinstance(val, str):
+                val = [int(x.strip()) for x in val.split(',')]
+            elif isinstance(val, int):
+                val = [val]
+            
+            for degree in val:                    
+                new_data_batch['inputs'].append(RandomRotation(degree)(batch_input))
+                new_data_batch['data_samples'].append(batch_data_sample)
+                
+        if key == 'Translate' and val:
+            if isinstance(val, str):
+                val = [int(x.strip()) for x in val.split(',')]
+            elif isinstance(val, int):
+                val = [val]
+                
+            new_data_batch['inputs'].append(translate_tensor(batch_input, val[0], val[1]))
+            new_data_batch['data_samples'].append(batch_data_sample)
+                    
+def recover_tta_output(augs, batch_output):
+    idx = 0
+    for key, val in augs.items():
+        if key == 'HorizontalFlip' and val:
+            batch_output[idx + 1].seg_logits = PixelData(data=RandomHorizontalFlip(1)(batch_output[idx + 1].seg_logits.data))
+            idx += 1
+    
+        if key == 'VerticalFlip' and val:
+            batch_output[idx + 1].seg_logits = PixelData(data=RandomVerticalFlip(1)(batch_output[idx + 1].seg_logits.data))
+            idx += 1
+            
+        if key == 'Rotate' and val:
+            if isinstance(val, str):
+                val = [int(x.strip()) for x in val.split(',')]
+            elif isinstance(val, int):
+                val = [val]
+            
+            for degree in val:                    
+                batch_output[idx + 1].seg_logits = PixelData(data=RandomRotation(360 - degree)(batch_output[idx + 1].seg_logits.data))
+                idx += 1
+                
+        if key == 'Translate' and val:
+            if isinstance(val, str):
+                val = [int(x.strip()) for x in val.split(',')]
+            elif isinstance(val, int):
+                val = [val]
+            
+            batch_output[idx + 1].seg_logits = PixelData(data=translate_tensor(batch_output[idx + 1].seg_logits.data, -val[0], -val[1]))
+            idx += 1
+            
+            
+
+class TTAModel(torch.nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self._model = model 
+    
+    def test_step(self, data_batch):
+
+        return self._model.test_step(data_batch)
+            
+    @property
+    def model(self):
+        return self._model 
+
+    @property
+    def cfg(self):
+        return self._cfg 
+    
+    def __getattr__(self, name):
+        if name in ['test_step', '_model']:
+            return super().__getattr__(name)
+        else:
+            return getattr(self._model, name)
+
 def translate_tensor(tensor, shift_x, shift_y):
     import torch.nn.functional as F
     _, h, w = tensor.shape
@@ -193,8 +307,6 @@ class TestLoopV2(TestLoop):
                 
                 with open(os.path.join(annotation_dir, filename + ".json"), "w") as jsf:
                     json.dump(_labelme, jsf)
-                
-
 
         return eval_cnt
     
@@ -220,13 +332,9 @@ class TestLoopV2(TestLoop):
             data_batch=data_batch,
             outputs=outputs)
     
-
-                           
                             
     @torch.no_grad()
     def run_tta_batch(self, data_batch):               
-        from torchvision.transforms import RandomHorizontalFlip, RandomVerticalFlip, RandomRotation
-        from mmengine.structures import PixelData
 
         augs = self.runner.cfg.tta['augs']
         batch_inputs = data_batch['inputs']
@@ -234,88 +342,11 @@ class TestLoopV2(TestLoop):
         
         new_data_batch = {'inputs': [], 'data_samples': []}
         batch_outputs = []
+        self.runner.model = TTAModel(self.runner.model)
         for batch_input, batch_data_sample in zip(batch_inputs, batch_data_samples):
-            new_data_batch['inputs'].append(batch_input)
-            new_data_batch['data_samples'].append(batch_data_sample)
-            for key, val in augs.items():
-                if key == 'HorizontalFlip' and val:
-                    new_data_batch['inputs'].append(RandomHorizontalFlip(1)(batch_input))
-                    new_data_batch['data_samples'].append(batch_data_sample)
-                
-                if key == 'VerticalFlip' and val:
-                    new_data_batch['inputs'].append(RandomVerticalFlip(1)(batch_input))
-                    new_data_batch['data_samples'].append(batch_data_sample)
-                    
-                if key == 'Rotate' and val:
-                    if isinstance(val, str):
-                        val = [int(x.strip()) for x in val.split(',')]
-                    elif isinstance(val, int):
-                        val = [val]
-                    
-                    for degree in val:                    
-                        new_data_batch['inputs'].append(RandomRotation(degree)(batch_input))
-                        new_data_batch['data_samples'].append(batch_data_sample)
-                        
-                if key == 'Translate' and val:
-                    if isinstance(val, str):
-                        val = [int(x.strip()) for x in val.split(',')]
-                    elif isinstance(val, int):
-                        val = [val]
-                        
-                    new_data_batch['inputs'].append(translate_tensor(batch_input, val[0], val[1]))
-                    new_data_batch['data_samples'].append(batch_data_sample)
-                    
+            make_tta_batch(augs, new_data_batch, batch_input, batch_data_sample)
             batch_output = self.runner.model.test_step(new_data_batch)
-            
-            idx = 0
-            for key, val in augs.items():
-                if key == 'HorizontalFlip' and val:
-                    batch_output[idx + 1].seg_logits = PixelData(data=RandomHorizontalFlip(1)(batch_output[idx + 1].seg_logits.data))
-                    idx += 1
-            
-                if key == 'VerticalFlip' and val:
-                    batch_output[idx + 1].seg_logits = PixelData(data=RandomVerticalFlip(1)(batch_output[idx + 1].seg_logits.data))
-                    idx += 1
-                    
-                if key == 'Rotate' and val:
-                    if isinstance(val, str):
-                        val = [int(x.strip()) for x in val.split(',')]
-                    elif isinstance(val, int):
-                        val = [val]
-                    
-                    for degree in val:                    
-                        batch_output[idx + 1].seg_logits = PixelData(data=RandomRotation(360 - degree)(batch_output[idx + 1].seg_logits.data))
-                        idx += 1
-                        
-                if key == 'Translate' and val:
-                    if isinstance(val, str):
-                        val = [int(x.strip()) for x in val.split(',')]
-                    elif isinstance(val, int):
-                        val = [val]
-                    
-                    batch_output[idx + 1].seg_logits = PixelData(data=translate_tensor(batch_output[idx + 1].seg_logits.data, -val[0], -val[1]))
-                    idx += 1
-            
-            seg_logits = batch_output[0].seg_logits.data
-            logits = torch.zeros(seg_logits.shape).to(seg_logits)
-            for data_sample in batch_output:
-                seg_logit = data_sample.seg_logits.data
-                if self.runner.model.out_channels > 1:
-                    logits += seg_logit.softmax(dim=0)
-                else:
-                    logits += seg_logit.sigmoid()
-            logits /= len(batch_output)
-            if self.runner.model.out_channels == 1:
-                seg_pred = (logits > self.runner.model.decode_head.threshold
-                            ).to(logits).squeeze(1)
-            else:
-                seg_pred = logits.argmax(dim=0)
-            data_sample.set_data({'seg_logits': PixelData(data=logits)})
-            data_sample.set_data({'pred_sem_seg': PixelData(data=seg_pred)})
-            if hasattr(batch_output[0], 'gt_sem_seg'):
-                data_sample.set_data(
-                    {'gt_sem_seg': batch_output[0].gt_sem_seg})
-            data_sample.set_metainfo({'img_path': batch_output[0].img_path})
-            batch_outputs.append(data_sample)
-            
+            recover_tta_output(augs, batch_output)
+            batch_outputs.append(merge_tta_batch(batch_output, self.runner.model.out_channels, self.runner.model.decode_head.threshold
+                                                 if hasattr(self.runner.model, 'decode_head') and hasattr(self.runner.model.decode_head, 'threshold') else None))
         return batch_outputs
