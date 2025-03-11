43fa735a30b780c778b20a23e6c69d3d2d2a0916
wonchul <kim.wonchul@aiv.ai>
2025-03-11 11:37:24 +0900

    split dinov2 into backbone and head configs

--- mm/segmentation/configs/_base_/models/dinov2/mask2former_ade20k-448x448.py
+++ mm/segmentation/configs/_base_/models/dinov2/mask2former_ade20k-448x448.py
@@ -1,13 +1,6 @@
-_base_ = [
-        '../../_base_/default_runtime.py', 
-        '../../_base_/datasets/ade20k-448x448.py',
-        '../../_base_/models/dinov2/mask2former_ade20k-448x448.py',
-]
 crop_size = (448, 448)
 num_classes = 150
-model = dict(
-    type="EncoderDecoder",
-    data_preprocessor=dict(
+data_preprocessor=dict(
         type="SegDataPreProcessor",
         mean=[123.675, 116.28, 103.53],
         std=[58.395, 57.12, 57.375],
@@ -15,29 +8,32 @@ model = dict(
         bgr_to_rgb=True,
         pad_val=0,
         seg_pad_val=255,
-    ),
+    )
+model = dict(
+    type="EncoderDecoder",
+    data_preprocessor=data_preprocessor,
     backbone=dict(
         type="DinoVisionTransformer",
-        patch_size=16,
-        embed_dim=1024,
-        depth=24,
-        num_heads=16,
+        patch_size=14,
+        embed_dim=384,
+        depth=12,
+        num_heads=6,
         mlp_ratio=4,
-        img_size=512,
+        img_size=224,
         ffn_layer="mlp",
         init_values=1e-05,
         block_chunks=0,
         qkv_bias=True,
         proj_bias=True,
         ffn_bias=True,
-        # init_cfg=dict(
-        #     type="Pretrained",
-        #     checkpoint="checkpoints/dinov2_converted.pth",
-        # ),
+        init_cfg=dict(
+            type="Pretrained",
+            checkpoint="/HDD/weights/dinov2/dinov2_vits14_pretrain.pth",
+        ),
     ),
     decode_head=dict(
         type="Mask2FormerHead",
-        in_channels=[1024, 1024, 1024, 1024],
+        in_channels=[384, 384, 384, 384],
         strides=[4, 8, 16, 32],
         feat_channels=256,
         out_channels=256,
@@ -158,50 +154,9 @@ model = dict(
     train_cfg=dict(),
     test_cfg=dict(
         mode="slide",
-        crop_size=(512, 512),
+        crop_size=crop_size,
         stride=(341, 341),
     ),
 )
 
 
-max_epochs = 1000
-embed_multi = dict(lr_mult=1.0, decay_mult=0.0)
-optim_wrapper = dict(
-    optimizer=dict(
-        type='AdamW',
-        lr=0.0001,
-        weight_decay=0.05,
-        eps=1e-08,
-        betas=(0.9, 0.999)),
-    paramwise_cfg=dict(
-        custom_keys=dict(
-            norm=dict(decay_mult=0.0),
-            query_embed=dict(lr_mult=1.0, decay_mult=0.0),
-            query_feat=dict(lr_mult=1.0, decay_mult=0.0),
-            level_embed=dict(lr_mult=1.0, decay_mult=0.0),
-            backbone=dict(lr_mult=0.1)),
-        norm_decay_mult=0.0)
-)
-param_scheduler = [
-    dict(type="PolyLR", eta_min=0, power=0.9, begin=0, end=40000, by_epoch=False)
-]
-
-
-default_hooks = dict(
-    checkpoint=dict(by_epoch=False, interval=20, type='CheckpointHook'),
-    logger=dict(interval=100, log_metric_by_epoch=False, type='LoggerHook'),
-    param_scheduler=dict(type='ParamSchedulerHook'),
-    sampler_seed=dict(type='DistSamplerSeedHook'),
-    timer=dict(type='IterTimerHook'),
-    visualization=dict(type='SegVisualizationHook', draw=True, interval=100))
-
-
-train_evaluator = dict(type='IoUMetric', iou_metrics=['mIoU', 'mDice', 'mFscore'],
-                     keep_results=True, classwise=True)
-val_evaluator = train_evaluator
-test_evaluator = dict(type='IoUMetric', iou_metrics=['mIoU', 'mDice', 'mFscore'],
-                     keep_results=True, classwise=True)
-
-train_cfg = dict(type="IterBasedTrainLoop", max_iters=40000, val_interval=10)
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
