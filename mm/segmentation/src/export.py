a605b3d964698e8025c6acccfb5772103da912b5
wonchul <kim.wonchul@aiv.ai>
2025-05-22 16:16:57 +0900

    update

--- mm/segmentation/src/export.py
+++ mm/segmentation/src/export.py
@@ -51,7 +51,7 @@ def main():
         model_checkpoint=args.checkpoint,
         device=args.device)
 
-def main2():
+def main1():
     
     # output_dir = '/DeepLearning/etc/_athena_tests/benchmark/tenneco/outer/outputs/m2f_epochs100/export'
     # model_cfg =  '/DeepLearning/etc/_athena_tests/benchmark/tenneco/outer/outputs/m2f_epochs100/train/mask2former_swin-s.py'
@@ -60,10 +60,13 @@ def main2():
     # model_cfg = "/DeepLearning/etc/_athena_tests/benchmark/tenneco/outer/outputs/pidnet_epochs100/train/pidnet_l.py" 
     # checkpoint = "/DeepLearning/etc/_athena_tests/benchmark/tenneco/outer/outputs/pidnet_epochs100/train/weights/best_mIoU_iter_23800.pth"
     
-    output_dir = "/DeepLearning/etc/_athena_tests/benchmark/tenneco/outer/outputs/sam2_epochs300/export"
-    model_cfg = "/DeepLearning/etc/_athena_tests/benchmark/tenneco/outer/outputs/sam2_epochs300/train/sam2_s.py" 
-    checkpoint = "/DeepLearning/etc/_athena_tests/benchmark/tenneco/outer/outputs/sam2_epochs300/train/weights/best_mIoU_iter_134318.pth"
+    # output_dir = "/DeepLearning/etc/_athena_tests/benchmark/tenneco/outer/outputs/sam2_epochs300/export"
+    # model_cfg = "/DeepLearning/etc/_athena_tests/benchmark/tenneco/outer/outputs/sam2_epochs300/train/sam2_s.py" 
+    # checkpoint = "/DeepLearning/etc/_athena_tests/benchmark/tenneco/outer/outputs/sam2_epochs300/train/weights/best_mIoU_iter_134318.pth"
     
+    output_dir = "/HDD/datasets/projects/Tenneco/Metalbearing/outer/250211/outputs/SEGMENTATION/segformer_b2_unfrozen"
+    model_cfg = "/HDD/datasets/projects/Tenneco/Metalbearing/outer/250211/outputs/SEGMENTATION/segformer_b2_unfrozen/segformer_mit-b2.py" 
+    checkpoint = "/HDD/datasets/projects/Tenneco/Metalbearing/outer/250211/outputs/SEGMENTATION/segformer_b2_unfrozen/weights/iter_100000.pth"
     
     parser = argparse.ArgumentParser(description='MMSeg export a model')
     args = parser.parse_args()
@@ -82,8 +85,10 @@ def main2():
     # args.backbone = 'swin-s'
     # args.model = 'pidnet'
     # args.backbone = 'l'
-    args.model = 'sam2'
-    args.backbone = 's'
+    # args.model = 'sam2'
+    # args.backbone = 's'
+    args.model = 'segformer'
+    args.backbone = 'mit-b2'
     args.height = 768
     args.width = 1120
     
@@ -112,4 +117,4 @@ def main2():
 
 if __name__ == '__main__':
     # main()
-    main2()
+    main1()
